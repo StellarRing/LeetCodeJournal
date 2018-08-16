fe59fe7a0fa4a4f4f30a597fac3c52b5b6fa51aa
wangcheng_bxbank <wangcheng@aibank.com>
2018-08-16 10:03:37 +0800

    ADD TwoSum

---
+++ src/Starter.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * Description:
+ *
+ * @author WangCheng
+ * @Date 2018/8/15 13:56
+ * @version 1.0
+ */
+public class Starter {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/algorithm/Algorithm00001.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+/**
+ * Description:
+ * 两数之和
+ *
+ * @author WangCheng
+ * @version 1.0
+ * @Date 2018/8/15 14:03
+ */
+public class Algorithm00001 {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
