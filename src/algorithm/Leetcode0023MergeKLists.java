811eb53bd4146a489d7ee680320c8eaa1a53c4cb
wangcheng_bxbank <wangcheng@aibank.com>
2018-08-31 22:58:44 +0800

    MergeKLists

---
+++ src/algorithm/Leetcode0023MergeKLists.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import algorithm.bean.ListNode;
+
+/**
+ * Description:
+ *
+ * @author WangCheng
+ * @version 1.0
+ * @Date 2018/8/26 16:34
+ */
+public class Leetcode0023MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode result = null;
+        if (lists == null || lists.length < 1) {
+            return null;
+        }
+        
+        return result;
+    }
+}
